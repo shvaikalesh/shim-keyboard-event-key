ec4d8f8e6d80da2b45f36768d3987438b021eb47
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-07-20 05:41:02 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+(function() {
+  "use strict"
+
+  var event = KeyboardEvent.prototype
+  var desc = Object.getOwnPropertyDescriptor(event, "key")
+  if (!desc) return
+
+  var keys = {
+    Win: "Meta",
+    Scroll: "ScrollLock",
+    Spacebar: " ",
+
+    Down: "ArrowDown",
+    Left: "ArrowLeft",
+    Right: "ArrowRight",
+    Up: "ArrowUp",
+
+    Del: "Delete",
+    Apps: "ContextMenu",
+    Esc: "Escape",
+
+    Multiply: "*",
+    Add: "+",
+    Subtract: "-",
+    Decimal: ".",
+    Divide: "/",
+  }
+
+  Object.defineProperty(event, "key", {
+    get: function() {
+      var key = desc.get.call(this)
+
+      return keys.hasOwnProperty(key) ? keys[key] : key
+    },
+  })
+})()
